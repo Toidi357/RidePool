34a9d606613c21cc0f6d06126ea551825d273c52
trash <toidi357@gmail.com>
2024-06-07 00:22:21 -0700

    send logging messages to stdout`

--- backend/app.py
+++ backend/app.py
@@ -17,7 +17,7 @@ from flask_migrate import Migrate
 migrate = Migrate(app, db)
 
 
-logging.basicConfig(filename = 'app.log', level = logging.DEBUG, format = '%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level = logging.DEBUG, format = '%(asctime)s - %(levelname)s - %(message)s')
 
 class Unauthorized(Exception):
     def __init__(self, message):
