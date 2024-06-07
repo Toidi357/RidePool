9548e1f870c3aee9b28ecd719fc755be5ee83f52
HarshPatil32 <patilsharsh@gmail.com>
2024-06-07 16:40:21 -0400

    Added ability to accept a requester

--- ridepool_mobile_frontend/app/components/UserList.js
+++ ridepool_mobile_frontend/app/components/UserList.js
@@ -1,26 +1,43 @@
 // UserList.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ScrollView, Text, StyleSheet } from 'react-native';
-import { Card, Paragraph } from 'react-native-paper';
+import { Card, Paragraph, Button } from 'react-native-paper';
+import { sendAuthorizedPostRequest, sendAuthorizedGetRequest } from "../components/sendRequest"
 
-const UserCard = ({ user }) => {
+
+const acceptRequester = async (requesterId, rideId) => {
+    try {
+        await sendAuthorizedPostRequest(`/rides/${rideId}/accept_requester/${requesterId}`);
+    } catch (err) {
+        console.error('Failed to accept requester: ', err);
+    }
+}
+
+const UserCard = ({ user, requesters = [], rideId }) => {
+    const isRequester = requesters.some(requester => requester.userId === user.userId);
     return (
         <Card style={styles.card}>
             <Card.Content>
+
                 <Text style={styles.userName}>{user.firstName} {user.lastName}</Text>
                 <Paragraph style={styles.userRating}>Username: {user.username}</Paragraph>
                 <Paragraph style={styles.userRating}>Rating: {user.averageRating}</Paragraph>
                 <Paragraph style={styles.userRating}>Phone Number: {user.phoneNumber}</Paragraph>
+                {isRequester && (
+                    <Card.Actions>
+                        <Button mode="contained" onPress={() => acceptRequester(user.userId, rideId)}>Accept</Button>
+                    </Card.Actions>
+                )}
             </Card.Content>
         </Card>
     );
 };
 
-const UserList = ({ users }) => {
+const UserList = ({ users, requesters = [], rideId }) => {
     return (
         <ScrollView style={styles.container}>
             {users.map((user, index) => (
-                <UserCard key={`user-${index}`} user={user} />
+                <UserCard key={`user-${index}`} user={user} requesters={requesters} rideId={rideId} />
             ))}
         </ScrollView>
     );
